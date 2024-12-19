94bde5a7cf88298c363ba1af7ed8fd81297afa1d
caleb <calebsg2253284@gmail.com>
2024-12-19 12:00:07 -0500

    initialize day 2 prob 1

---
+++ day_02/prob_1/src/main.rs
@@ -0,0 +1,16 @@
+/*
+
+- input contains lines of text
+- each line contains a number of space-separated intergers representing levels
+
+- for a level to be considered 'safe':
+    1) the levels must all either increase or decrease
+    2) the distance between 2 consecutive levels must be between 1 and 3 (inclusive)
+
+Given these requirements, calculate the number of 'safe' levels in the input
+
+*/
+
+fn main() {
+    
+}
